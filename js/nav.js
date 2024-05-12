08c3967ecd9a5770aad38acfa0309e0e18f71e86
jsan712 <jsan712@gmail.com>
2024-05-12 14:25:21 -0500

    Correcting nav bar display

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,1 @@
-$(function () {$("#nav-placeholder").load("/nav.html");});
-// TODO: dynamic loading of nav from different directories
-// This will also affect how nav.html loads its files
+$(function () {$("#nav-placeholder").load("../nav.html");});
